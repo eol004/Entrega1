61843a8dc4ebfc81d4cb487af15b96b7d43038ca
Estefania <estefaniaonatell01@gmail.com>
2023-03-20 00:20:53 +0100

    solución fallo al hacer apk

--- app/src/main/java/com/example/myapplication/Inicio.java
+++ app/src/main/java/com/example/myapplication/Inicio.java
@@ -6,21 +6,30 @@ import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Inicio extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inicio);
-
-        new Handler().postDelayed(new Runnable() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.submit(new Runnable() {
             @Override
             public void run() {
+                try {
+                    Thread.sleep(5000); // 5000 milisegundos = 5 segundos
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 Intent intent = new Intent(Inicio.this, Registro.class);
                 startActivity(intent);
                 finish();
             }
-        }, 5000); // 5000 milisegundos = 5 segundos
+        });
+
     }
 
 }
